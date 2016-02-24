8982242b771849b76b0b3d6fc2772ef9f027c110
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-02-24 16:24:55 +0100

    working directive  scope

--- app/app.js
+++ app/app.js
@@ -118,3 +118,52 @@ app.directive('address', function () {
     }
 });
 
+////////////////////////////////////////////////////////
+app.controller('decoratorCtrl', function () {
+    var vm = this;
+
+    vm.messages = [];
+    vm.handlePause = function (e) {
+        console.log(e);
+        vm.messages.push({text: 'paused'});
+        console.log('paused !');
+    }
+});
+
+app.directive('eventPause', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, el, attrs) {
+            var fn = $parse(attrs['eventPause']);
+            /**
+             * Event is fired outside of Angular digest circle.
+             * Event = pause event of video element
+             */
+            el.on('pause', function (event) {
+                scope.$apply(function () {
+                    fn(scope, {evt: event});
+                });
+            });
+        }
+    }
+});
+
+app.directive('spacebarSupport', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, el, attrs) {
+            $('body').on('keypress', function (evt) {
+                var vidEl = el[0];
+                if (evt.keyCode === 32) {
+                    if (vidEl.paused) {
+                        vidEl.play();
+                    } else {
+                        vidEl.pause();
+                    }
+                }
+            })
+        }
+
+    }
+});
+
