fc05e845af5799b4424916a6a05cb7394831b170
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-02-24 13:37:33 +0100

    working directive in isolate scope

--- app/app.js
+++ app/app.js
@@ -1,12 +1,58 @@
-'use strict';
+var app = angular.module('app', []);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+app.controller('mainCtrl', function () {
+    var vm = this;
+    vm.user1 = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rebel Base',
+            planet: 'Yavin 4',
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    vm.user2 = {
+        name: 'Han Solo',
+        address: {
+            street: 'Kerkstraat',
+            city: 'Brussel',
+            planet: 'Yavin 4',
+        },
+        friends: [
+            'Luke',
+            'Chewbacca',
+            'Leia',
+        ]
+    };
+
+    console.log('Scope', vm);
+
+});
+
+/**
+ * bindToController:
+ * http://blog.thoughtram.io/angularjs/2015/01/02/exploring-angular-1.3-bindToController.html
+ */
+app.directive('userInfoCard', function () {
+    return {
+        templateUrl: './templates/userInfoCard.html',
+        restrict: 'E',
+        scope: {},
+        bindToController: {
+            user: '='
+        },
+        controller: function () {
+            var userVm = this;
+            userVm.knightMe = function (user) {
+                user.rank = 'Knight';
+            }
+        },
+        controllerAs: 'userVm'
+    }
+
+});
